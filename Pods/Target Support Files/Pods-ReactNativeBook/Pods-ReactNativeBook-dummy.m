36fa579f39aabea338b4c18c3d5de7850b275ada
Xcoder1011 <shangkunwu@msn.com>
2022-12-15 17:31:35 +0800

    reactor:  --template use react-native-template-typescript.

--- Pods/Target Support Files/Pods-ReactNativeBook/Pods-ReactNativeBook-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeBook : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeBook
-@end
--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactNativeBook
-mvvm+redux+react native
